ee0b686544b767b782b6100d6c9cde310b124e62
tastelikecoke <jmerc87@gmail.com>
2025-05-30 00:02:02 +0800

    Add 9 slice and proper deck display

--- Assets/Scripts/DeckDisplay.cs
+++ Assets/Scripts/DeckDisplay.cs
@@ -1,16 +1,20 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeckDisplay : MonoBehaviour
 {
 
     [SerializeField]
     private TMP_Text deckText;
+    [SerializeField]
+    private Image deckImage;
 
     public void Populate(List<Card> cards)
     {
         deckText.text = cards.Count.ToString();
+        deckImage.fillAmount = Mathf.Clamp01(cards.Count / 40f);
     }
 
 }
